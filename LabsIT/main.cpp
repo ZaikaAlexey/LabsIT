db5cff0d34ecb4b543f0e0f48a766752bab5f11f
Алексей Заика <sekval.2605.2004@gmail.com>
2025-06-04 05:34:36 +0300

    Добавил параметр "Рост" и пофиксил баг с выводом данных на кирилице

--- LabsIT/main.cpp
+++ LabsIT/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <clocale>
+#include <cstdlib>
 #include <string>
+
+#include <windows.h>
 
 using namespace std;
 
@@ -8,26 +12,29 @@ class User
 public:
 	string name;
 	int age;
-	
+	int height;
 
 private:
 
 };
 void print(User text) {
 	cout << " \n ************************** \n";
-	cout << "Имя: " << text.name << endl;
-	cout << "Возраст: " << text.age << endl;
+	cout << "Имя: " << text.name <<endl;
+	cout << "Возраст: " << text.age << "лет" << endl;
+	cout << "Рост:" << text.height << "см." << endl;
 };
 
 
 int main() {
-	setlocale(LC_ALL, "RUS");
+	system("chcp 1251 >> null"); // для русификации вывода 
+	setlocale(LC_ALL, "RUS"); // для кирилицы 
 	cout << "===============\n Привет ВГЛТУ! \n===============" << endl;
 	cout << " \nЗаполните поля:" << endl;
 	cout << "_________________________" << endl;
 	User student;
-	cout << "Имя: "; cin >> student.name;
-	cout << "Возраст: "; cin >> student.age;
+	cout << "Имя: "; cin >> student.name; 
+	cout << "Возраст: "; cin >> student.age; 
+	cout << "Рост: "; cin >> student.height;
 	print(student);
 	
 	return 0;
