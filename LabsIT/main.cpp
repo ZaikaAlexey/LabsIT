8671a422fb9cad05feeb1098b815c117ada4ac22
Алексей Заика <sekval.2605.2004@gmail.com>
2025-06-04 05:01:47 +0300

    Добавлен класс с вводимыми данными и их вывод на экран.

--- LabsIT/main.cpp
+++ LabsIT/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
+class User
+{
+public:
+	string name;
+	int age;
+	
+
+private:
+
+};
+void print(User text) {
+	cout << " \n ************************** \n";
+	cout << "Имя: " << text.name << endl;
+	cout << "Возраст: " << text.age << endl;
+};
+
+
 int main() {
 	setlocale(LC_ALL, "RUS");
-	cout << "Привет ВГЛТУ!" << endl;
+	cout << "===============\n Привет ВГЛТУ! \n===============" << endl;
+	cout << " \nЗаполните поля:" << endl;
+	cout << "_________________________" << endl;
+	User student;
+	cout << "Имя: "; cin >> student.name;
+	cout << "Возраст: "; cin >> student.age;
+	print(student);
+	
 	return 0;
 }
